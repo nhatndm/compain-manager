ef5b9ee9d6607b5ad72c1de689ddfedee2a3e150
Nhat Nguyen <sin@Cu-Sin-MacBook-M4.local>
2026-04-09 00:12:32 +0800

    fix: error for knex migration

--- apps/api/src/database/knex.config.ts
+++ apps/api/src/database/knex.config.ts
@@ -1,6 +1,6 @@
 import type { Knex } from 'knex'
 
-export const knexConfig: Knex.Config = {
+const knexConfig: Knex.Config = {
   client: 'postgresql',
   connection: process.env['DATABASE_URL'] ?? 'postgresql://localhost:5432/compain_manager',
   migrations: {
@@ -8,3 +8,5 @@ export const knexConfig: Knex.Config = {
     extension: 'ts',
   },
 }
+
+export default knexConfig
